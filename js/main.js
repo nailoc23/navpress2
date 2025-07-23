0c021f512d76566c3fb3ead09d4dce550c44b293
nailoc23 <nailoc23@naver.com>
2025-07-23 10:52:33 +0900

    공통영역수정: 모바일 메뉴 닫힘 수정

--- js/main.js
+++ js/main.js
@@ -38,5 +38,22 @@ Author: GrayGrids
         navbarToggler.classList.toggle("active");
     });
 
+    document.addEventListener('click', function (event) {
+        const menu = document.querySelector('#navbarSupportedContent'); // 메뉴 영역
+        const toggler = document.querySelector('.mobile-menu-btn');     // 햄버거 버튼
+
+        const isClickInsideMenu = menu.contains(event.target);
+        const isClickOnToggler = toggler.contains(event.target);
+
+        // 메뉴가 열려있고, 메뉴 및 햄버거 버튼이 아닌 다른 곳을 클릭한 경우
+        if (menu.classList.contains('show') && !isClickInsideMenu && !isClickOnToggler) {
+            // Bootstrap 5의 collapse를 수동으로 닫기
+            const bsCollapse = bootstrap.Collapse.getInstance(menu);
+            if (bsCollapse) {
+                bsCollapse.hide();
+            }
+        }
+    });
+
 
 })();
